a86c4611f594f932c3861394774f41f56bd3f266
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-04-21 20:27:07 +0900

    Styled DateSelector

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -3,7 +3,8 @@ import moment from "moment";
 import React from "react";
 import "antd/dist/antd.css";
 import calendarIcon from "../assets/icon_calender.svg";
-
+import searchIcon from "../assets/icon_search.svg";
+import "../styles/dataSelector.css";
 function DateSelector() {
   const { RangePicker } = DatePicker;
   function onChange(dates: any[] | null, dateStrings: any[] | null) {
@@ -13,19 +14,26 @@ function DateSelector() {
     }
   }
   return (
-    <Space style={{ borderRadius: "10%" }}>
-      <img src={calendarIcon} style={{ width: "2em" }} />
-      <RangePicker
-        bordered={false}
-        size={"large"}
-        suffixIcon={false}
-        ranges={{
-          Today: [moment(), moment()],
-          "This Month": [moment().startOf("month"), moment().endOf("month")],
-        }}
-        onChange={onChange}
-      />
-    </Space>
+    <React.Fragment>
+      <Space className="container" size="middle">
+        <img alt="calendar-icon" src={calendarIcon} className="calendar" />
+        <RangePicker
+          defaultValue={[moment().startOf("month"), moment().endOf("month")]}
+          className="range-picker"
+          bordered={false}
+          size={"large"}
+          suffixIcon={false}
+          ranges={{
+            Today: [moment(), moment()],
+            "This Month": [moment().startOf("month"), moment().endOf("month")],
+          }}
+          onChange={onChange}
+        />
+      </Space>
+      <Space className="search-container">
+        <img alt="search-icon" src={searchIcon} className="search" />
+      </Space>
+    </React.Fragment>
   );
 }
 
