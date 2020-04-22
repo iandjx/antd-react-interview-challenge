b5af1b50347690528c2f296d739753b2f70f0602
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-04-22 20:39:00 +0900

    Added boiller plate code for EmailGrid

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import DateSelector from "./components/DateSelector";
 import EmailCounter from "./components/EmailCounter";
+import EmailGrid from "./components/EmailGrid";
 function App() {
   return (
     <div className="App">
       <DateSelector />
       <EmailCounter />
+      <EmailGrid />
     </div>
   );
 }
---
+++ src/components/EmailGrid.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Column, useTable } from "react-table";
+
+import makeData from "../makeData";
+
+interface ITable<T extends object> {
+  columns: Column<T>[];
+  data: T[];
+}
+
+function Table<T extends object>({ columns, data }: ITable<T>) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable<T>({
+    columns,
+    data,
+  });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function EmailGrid() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        columns: [
+          {
+            Header: "First Name",
+            accessor: "firstName",
+          },
+          {
+            Header: "Last Name",
+            accessor: "lastName",
+          },
+        ],
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Age",
+            accessor: "age",
+          },
+          {
+            Header: "Visits",
+            accessor: "visits",
+          },
+          {
+            Header: "Status",
+            accessor: "status",
+          },
+          {
+            Header: "Profile Progress",
+            accessor: "progress",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => makeData(20), []);
+
+  return <Table columns={columns} data={data} />;
+}
+
+export default EmailGrid;
