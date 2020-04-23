91ca4493becba3edbfe682a81d73600f9eef41f7
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-04-23 15:44:09 +0900

    Added filter logic
    filtering can't be done outside of the table https://github.com/tannerlinsley/react-table/issues/944

--- src/components/EmailGrid.tsx
+++ src/components/EmailGrid.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Column, useTable, useSortBy } from "react-table";
+import {
+  Column,
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+} from "react-table";
 
 import makeData from "../makeData";
 
@@ -8,7 +14,70 @@ interface ITable<T extends object> {
   data: T[];
 }
 
+const DefaultColumnFilter = ({
+  column: { filterValue, preFilteredRows, setFilter },
+}: any) => {
+  const count = preFilteredRows.length;
+
+  return (
+    <input
+      value={filterValue || ""}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  );
+};
+
+const GlobalFilter = ({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}: any) => {
+  const count = preGlobalFilteredRows && preGlobalFilteredRows.length;
+
+  return (
+    <span>
+      Search:{" "}
+      <input
+        value={globalFilter || ""}
+        onChange={(e) => {
+          setGlobalFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          border: "0",
+        }}
+      />
+    </span>
+  );
+};
+
 function Table<T extends object>({ columns, data }: ITable<T>) {
+  const filterTypes = React.useMemo(
+    () => ({
+      text: (rows: any, id: any, filterValue: any) => {
+        return rows.filter((row: any) => {
+          const rowValue = row.values[id];
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true;
+        });
+      },
+    }),
+    []
+  );
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  );
+
   // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
@@ -16,11 +85,21 @@ function Table<T extends object>({ columns, data }: ITable<T>) {
     headerGroups,
     rows,
     prepareRow,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
   } = useTable<T>(
     {
       columns,
       data,
+      defaultColumn,
+      filterTypes,
     },
+
+    useFilters,
+
+    useGlobalFilter,
     useSortBy
   );
 
@@ -33,13 +112,29 @@ function Table<T extends object>({ columns, data }: ITable<T>) {
             {headerGroup.headers.map((column) => (
               <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                 {column.render("Header")}
+
                 <span>
                   {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""}
                 </span>
+                <div>{column.canFilter ? column.render("Filter") : null}</div>
               </th>
             ))}
           </tr>
         ))}
+        <tr>
+          <th
+            colSpan={visibleColumns.length}
+            style={{
+              textAlign: "left",
+            }}
+          >
+            <GlobalFilter
+              preGlobalFilteredRows={preGlobalFilteredRows}
+              globalFilter={state.globalFilter}
+              setGlobalFilter={setGlobalFilter}
+            />
+          </th>
+        </tr>
       </thead>
       <tbody {...getTableBodyProps()}>
         {rows.map((row, i) => {
@@ -63,19 +158,23 @@ function EmailGrid() {
       {
         Header: "From",
         accessor: "from",
+        filter: "text",
       },
 
       {
         Header: "To",
         accessor: "to",
+        filter: "text",
       },
       {
         Header: "Subject",
         accessor: "subject",
+        filter: "text",
       },
       {
         Header: "Date",
         accessor: "date",
+        filter: "text",
       },
     ],
     []
