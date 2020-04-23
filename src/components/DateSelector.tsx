7e23ff8f65e54f8cd0dbaa811344e9f19d2dc459
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-04-24 05:49:18 +0900

    Added state logic on DateSelector

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,20 +1,43 @@
 import { DatePicker, Space } from "antd";
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
 import "antd/dist/antd.css";
 import calendarIcon from "../assets/icon_calender.svg";
 import searchIcon from "../assets/icon_search.svg";
+import { useDispatch } from "react-redux";
+
+export interface Ranges {
+  fromDate: moment.Moment;
+  toDate: moment.Moment;
+}
 
 function DateSelector() {
+  // const dispatch = useDispatch();
+  const [range, setRange] = useState<Ranges>({
+    fromDate: moment(),
+    toDate: moment(),
+  });
+
   const { RangePicker } = DatePicker;
+
   function onChange(dates: any[] | null, dateStrings: any[] | null) {
     if (dates !== null && dateStrings !== null) {
+      setRange({
+        ...range,
+        fromDate: dates[0],
+        toDate: dates[1],
+      });
+
       console.log("From: ", dates[0], ", to: ", dates[1]);
       console.log("From: ", dateStrings[0], ", to: ", dateStrings[1]);
     }
   }
   return (
     <React.Fragment>
+      {console.log(range)}
+      <p>
+        {range.fromDate.toLocaleString} I am {range.toDate.toString}
+      </p>
       <Space className="container" size="middle">
         <img alt="calendar-icon" src={calendarIcon} className="calendar" />
         <RangePicker
@@ -31,7 +54,12 @@ function DateSelector() {
         />
       </Space>
       <Space className="search-container">
-        <img alt="search-icon" src={searchIcon} className="search" />
+        <img
+          alt="search-icon"
+          src={searchIcon}
+          className="search"
+          onClick={() => console.log("test")}
+        />
       </Space>
     </React.Fragment>
   );
