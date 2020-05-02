db6b42eba852361f7f62fd343af4362607dd4729
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-05-03 06:15:14 +0900

    Sort data descending based on from email

--- src/components/EmailSorter.tsx
+++ src/components/EmailSorter.tsx
@@ -1,18 +1,51 @@
 import React from "react";
 import { Button } from "antd";
+import { useSelector } from "react-redux";
 
 export default function EmailSorter() {
+  const [from, setFrom] = React.useState("");
+  const [to, setTo] = React.useState("");
+  const [subject, setSubject] = React.useState("");
+  const [date, setDate] = React.useState("");
+
+  const sort = (header: string) => {};
+  const emailData = useSelector((state: any) => state.filter.emails);
+
+  const sortSwitch = (header: any) => {
+    switch (header) {
+      case "from":
+        const sortedData = [...emailData];
+        sortedData.sort((a: any, b: any) => {
+          return a.from > b.from ? 1 : -1;
+        });
+        console.log(sortedData);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="site-button-ghost-wrapper">
-      <Button type="primary" ghost>
-        Primary
+      <Button
+        type="link"
+        onClick={() => sortSwitch("from")}
+        ghost
+        style={{ color: "rgb(136, 136, 136)" }}
+      >
+        From
       </Button>
-      <Button ghost>Default</Button>
-      <Button type="dashed" ghost>
-        link
+      |
+      <Button type="link" ghost style={{ color: "rgb(136, 136, 136)" }}>
+        To
       </Button>
-      <Button type="link" ghost>
-        link
+      |
+      <Button type="link" ghost style={{ color: "rgb(136, 136, 136)" }}>
+        Subject
+      </Button>
+      |
+      <Button type="link" ghost style={{ color: "rgb(136, 136, 136)" }}>
+        Date
       </Button>
     </div>
   );
