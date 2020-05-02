454c87cc408402ab053394a0a2bc12f936a163c9
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-05-03 05:31:09 +0900

    Fixed email card styling

--- src/components/EmailCard.tsx
+++ src/components/EmailCard.tsx
@@ -3,6 +3,7 @@ import { Card, Tag } from "antd";
 import previewIcon from "../assets/icon_mail_sp.svg";
 
 import { Typography } from "antd";
+import clipIcon from "../assets/icon_clip.svg";
 
 const { Text } = Typography;
 
@@ -12,34 +13,78 @@ export default function EmailCard(props: any) {
   let toEmail: any;
   if (email.to.length > 2) {
     toEmail = (
-      <div style={{ display: "flex" }} className="tag-container">
-        <Text>
+      <div style={{ display: "flex" }} className="mobile-tag-container">
+        <Text
+          style={{
+            width: 300,
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
           {/* {console.log("second" + email.to[1].address)} */}
-          {email.to[0].address}, {email.to[1].address} ...
+          {email.to[0].address}, {email.to[1].address}
         </Text>
         <Tag className="to-counter">+{email.to.length - 1}</Tag>
       </div>
     );
   } else {
     toEmail = (
-      <Text>
+      <Text
+        style={{
+          width: 100,
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+        }}
+      >
         {email.to[0].address}, {email.to[1] ? email.to[1].address : ""}
       </Text>
     );
   }
 
+  const attachment = email.hasAttachment ? (
+    <img
+      alt="clip-icon"
+      src={clipIcon}
+      className="mobile-clip-icon"
+      style={{ marginLeft: "auto" }}
+    />
+  ) : (
+    ""
+  );
+
   return (
     <div>
-      <Card size="small" style={{ width: 300 }}>
-        <div style={{ display: "flex" }}>
+      <Card size="small" style={{ width: 450 }}>
+        <div style={{ display: "flex", width: "100%" }}>
           <img src={previewIcon} alt="preview icon" style={{ width: "1em" }} />
-          <div style={{ marginLeft: "1em" }}>
-            <Text strong>{email.from} </Text>
-            {toEmail}
+          <div style={{ marginLeft: "1em", flexGrow: 2 }}>
+            <div style={{ display: "flex" }}>
+              <Text strong>{email.from} </Text>
+              {attachment}
+              <Text
+                style={{
+                  marginRight: "0.5em",
+                  textAlign: "center",
+                }}
+              >
+                time
+              </Text>
+            </div>
+            <div>{toEmail}</div>
           </div>
-          <div style={{ marginLeft: "auto" }}>time</div>
         </div>
-        <div>{email.body}</div>
+        <div
+          style={{
+            width: "95%",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {email.body}
+        </div>
       </Card>
     </div>
   );
